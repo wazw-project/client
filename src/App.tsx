24b733f9022cb207b0cd21d13704e1b7108bb8bb
naamafrank1 <naama0556772275@gmail.com>
2022-08-04 18:12:07 +0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import User from './components/UserPage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-    <User/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='' element={<User />} />
+      </Routes>
+    </Router>
+
   );
 }
 

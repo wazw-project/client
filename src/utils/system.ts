d08c6118e80c3c7e7aec3f24d90fc36504147f6c
tamarOstry <tamar32410@gmail.com>
2022-08-09 15:54:37 +0300

    get all system of manager

---
+++ src/utils/system.ts
@@ -0,0 +1,11 @@
+export interface System{
+    id: string,
+    topic: string,
+    objectName: string,
+    managerUid: string,
+    description: string,
+    email: string,
+    phone: string,
+    urlName: string,
+    urlImage: string
+}

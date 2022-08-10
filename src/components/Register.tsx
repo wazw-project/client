36de08136bbd9e9f14e712e9257270d6eb05fbd8
tamarOstry <tamar32410@gmail.com>
2022-08-10 20:07:42 +0300

    login user also to the db

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
+import { User } from '../utils/user';
 import axios from 'axios';
 import {
   auth,
@@ -12,28 +13,33 @@ import "../style/Register.css";
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
   const [user, loading, error] = useAuthState(auth);
-  // const [res,setRes]  = useState<any>();
+  const [res,setRes]  = useState<any>();
+  const [userFromDb,setUserFromDb]  = useState<any>();
   const navigate = useNavigate();
-  const register = () => {
-    if (!name) alert("Please enter name");
-    registerWithEmailAndPassword(name, email, password);
-    // const resFromFire=await registerWithEmailAndPassword(name, email, password);
-    // setRes(resFromFire);
+  const register = async() => {
+    if (!firstName) alert("Please enter firstName");
+    const fullName = firstName+' '+lastName;
+    await registerWithEmailAndPassword(fullName, email, password);
+    await addUserToDb();
   };
 
-  // useEffect(() => {
-  //   addUserToDb();
-  // },[res])
-
-  // const addUserToDb=async()=>{
-  //   try {
-  //     const res = await axios.post(`http://localhost:3333/user/addUser`,{});
-  //     let tempList = await res.data;
-  //     setSystems(tempList);
-  // } catch (error) { console.log(error); }
-  // }
+  const addUserToDb=async()=>{
+    const userToDb={
+      "firstName":firstName,
+      "lastName":lastName,
+      "phone":phone,
+      "email":email
+    }    
+    try {
+      const res = await axios.post(`http://localhost:3333/user/addUser`,userToDb);
+      let tempList = await res.data;
+      setUserFromDb(tempList);
+  } catch (error) { console.log(error); }
+  }
 
   useEffect(() => {
     if (loading) return;
@@ -45,9 +51,23 @@ function Register() {
         <input
           type="text"
           className="register__textBox"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Full Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="firstName"
+        />
+        <input
+          type="text"
+          className="register__textBox"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder="lastName"
+        />
+        <input
+          type="text"
+          className="register__textBox"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="phone"
         />
         <input
           type="text"
---
+++ src/utils/user.ts
@@ -0,0 +1,8 @@
+export interface User{
+    _id?: any,
+    role?: string,
+    firstName: string,
+    lastName: string,
+    phone: string,
+    email: string,
+}

cb899da9ec2700e6c74eedf26f4929887f1ebd50
naamafrank1 <naama0556772275@gmail.com>
2022-08-14 04:52:06 +0300

    test

---
+++ src/components/systems.test.jsx
@@ -0,0 +1,51 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Systems from "./systems";
+import { createMemoryHistory } from "history";
+
+
+describe("<Systems />", () => {
+    let container = null;
+    beforeEach(() => {
+        // setup a DOM element as a render target
+        container = document.createElement("div");
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        // cleanup on exiting
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+    });
+    test("Should render system correctly with details", () => {
+         const system =
+
+        {
+            _id: "62f834aae04fc9cb513b8a2c",
+            topic: "Wheelchairs",
+            objectName: "Wheelchair",
+            managerUid: "62f833c8e04fc9cb513b8a22",
+            description: "here you can rent Wheelchair",
+            email: "naama@gmail.com",
+            phone: "0556772275",
+            urlName: "Wheelchair",
+            urlImage: "https://www.animatedimages.org/data/media/1655/animated-wheelchair-image-0017.gif",
+            __v: 0
+
+        }
+        const history = createMemoryHistory();
+        const state = { id: "62f833c8e04fc9cb513b8a22" }
+        history.push("/", state);
+        render(<BrowserRouter>
+            <Routes history={history}>
+                <Route path="*" element={<Systems />} />
+            </Routes>
+        </BrowserRouter>, container);
+
+        expect(container.textContent).toContain("log out");
+        expect(container.textContent).toContain("All systems");
+        expect(container.textContent).toContain("add system");
+
+    });
+});

f8c0366f3b14cce15d9ebfa18b978475e4d5c803
tamarOstry <tamar32410@gmail.com>
2022-08-21 17:10:02 +0300

    mobix aa

--- src/store.ts
+++ src/store.ts
@@ -3,41 +3,78 @@ import { System } from './utils/system';
 import { makeAutoObservable } from 'mobx';
 import axios from 'axios';
 
-const addSystem=async(system:System)=>{
-    try {     
-        const res = await axios.post(`http://localhost:3333/system/addSystem`,system);
+const addSystem = async (system: System) => {
+    try {
+        const res = await axios.post(`http://localhost:3333/system/addSystem`, system);
         const data = await res.data;
         console.log(data);
         return data;
     } catch (error) { console.log(error); }
 }
 
-const getSystems=async()=>{
+const getSystems = async () => {
     try {
-        const managerId='62f4bec1c9f7408b6d78e779';
+        const managerId = '62f4bec1c9f7408b6d78e779';
         const res = await axios.get(`http://localhost:3333/system/${managerId}`);
         let tempList = await res.data;
         return tempList;
     } catch (error) { console.log(error); }
 }
-   
 
-class Store{
-    user: any=null;
-    systems:System[]=[];
+const removeSystem = async (systemId: string) => {
+    try {
+        await axios.delete(` http://localhost:3333/system/${systemId}`)
+    } catch (error) { console.log(error); }
+}
 
-    async loadSystems(){
+const editSystem = async (managerId: string, system: System) => {
+    try {
+        const res = await axios.put(` http://localhost:3333/system/${managerId}`, system)
+        console.log(res.data)
+    } catch (error) { console.log(error); }
+}
+
+const getSystemById = async (id: string) => {
+    try {
+        const res = await axios.get(` http://localhost:3333/system/systemById/${id}`)
+        const data = await res.data;
+        return data;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+class Store {
+    user: any = null;
+    systems: System[] = [];
+    // currentSystem
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    async loadSystems() {
         this.systems = await getSystems();
     }
 
-    async addSystem(system:System){
+    async addSystem(system: System) {
         await addSystem(system);
         this.systems.push(system);
         console.log(this.systems)
     }
 
-    constructor(){
-        makeAutoObservable(this);
+    async removeSystem(systemId: string) {
+        await removeSystem(systemId);
+        this.systems = this.systems.filter(system => system._id !== systemId);
+    }
+
+    async editSystem(managerId: string, system: System) {
+        await editSystem(managerId, system);
+        // this.systems=this.systems.filter(s=>s._id === system._id?s==system:s);
+    }
+
+    async getSystemById(Id: string) {
+        const system = await getSystemById(Id);
+
     }
 }
 

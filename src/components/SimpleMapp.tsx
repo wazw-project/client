580a5661122205e334ceb3520738faf7cb804ea6
naamafrank1 <naama0556772275@gmail.com>
2022-08-24 00:05:16 +0300

    size of map

--- src/components/SimpleMapp.tsx
+++ src/components/SimpleMapp.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 import Box from '@mui/material/Box';
@@ -9,6 +9,7 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
 import markerStore from '../store/markerStore';
+import { height } from '@mui/system';
 
 const mapStyles = {
   width: '20%',
@@ -18,35 +19,29 @@ interface Film {
   title: string;
   year: number;
 }
-
 function sleep(delay = 0) {
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
 }
-
-
-
 const SimpleMap: React.FC = (props: any) => {
   const getMapOptions = (maps: any) => {
     return {
       disableDefaultUI: true,
       mapTypeControl: true,
       streetViewControl: true,
-      styles: [{  height: '90%', featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
-      
+      styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
+
     };
   };
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState<readonly Film[]>([]);
   const loading = open && options.length === 0;
-  React.useEffect(() => {
+  useEffect(() => {
     let active = true;
-
     if (!loading) {
       return undefined;
     }
-
     (async () => {
       await sleep(1e3); // For demo purposes.
 
@@ -54,13 +49,11 @@ const SimpleMap: React.FC = (props: any) => {
         setOptions([...topFilms]);
       }
     })();
-
     return () => {
       active = false;
     };
   }, [loading]);
-
-  React.useEffect(() => {
+  useEffect(() => {
     if (!open) {
       setOptions([]);
     }
@@ -68,71 +61,66 @@ const SimpleMap: React.FC = (props: any) => {
   const [center, setCenter] = useState({ lat: 11.0168, lng: 76.9558 });
   const [zoom, setZoom] = useState(11);
   return (
-    <>
-      <Box sx={{ flexGrow: 1,
-    }} >
-        <Grid container spacing={1} >
-          <Grid   item md={8}  >
-            <GoogleMapReact  
-                  
-              bootstrapURLKeys={{ key: 'AIzaSyAcibzCa3ilUV5eZNEQpjqLmWzdm35tymw' }}
-              defaultCenter={center}
-              defaultZoom={zoom}
-              options={getMapOptions}
 
-            >
-         
-              {markerStore.markers.map(m=>( 
-                <Marker
-                  lat={m.lat}
-                  lng={m.lng}
-                  name={m.name}
-                  color={m.color}
-                />
-              ))}
+      <Grid container spacing={2} height={592}>
+        <Grid item xs={6} md={8}>
+          <GoogleMapReact
 
-            </GoogleMapReact>
-          </Grid>
-          <Grid item xs={6} md={4}>
-            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h4" component="div">
-              here you can search  location business of your system
-            </Typography>
+            bootstrapURLKeys={{ key: 'AIzaSyAcibzCa3ilUV5eZNEQpjqLmWzdm35tymw' }}
+            defaultCenter={center}
+            defaultZoom={zoom}
+            options={getMapOptions}
+          >
 
-            <Autocomplete
-              id="asynchronous-demo"
-              sx={{ width: 300 }}
-              open={open}
-              onOpen={() => {
-                setOpen(true);
-              }}
-              onClose={() => {
-                setOpen(false);
-              }}
-              isOptionEqualToValue={(option, value) => option.title === value.title}
-              getOptionLabel={(option) => option.title}
-              options={options}
-              loading={loading}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Asynchronous"
-                  InputProps={{
-                    ...params.InputProps,
-                    endAdornment: (
-                      <React.Fragment>
-                        {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                        {params.InputProps.endAdornment}
-                      </React.Fragment>
-                    ),
-                  }}
-                />
-              )}
-            />
+            {markerStore.markers.map(m => (
+              <Marker
+                lat={m.lat}
+                lng={m.lng}
+                name={m.name}
+                color={m.color}
+              />
+            ))}
 
-          </Grid>
+          </GoogleMapReact>
         </Grid>
-      </Box>
-    </>
+        <Grid item xs={6} md={4}>
+          <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h4" component="div">
+            here you can search  location business of your system
+          </Typography>
+
+          <Autocomplete
+            id="asynchronous-demo"
+            sx={{ width: 300 }}
+            open={open}
+            onOpen={() => {
+              setOpen(true);
+            }}
+            onClose={() => {
+              setOpen(false);
+            }}
+            isOptionEqualToValue={(option, value) => option.title === value.title}
+            getOptionLabel={(option) => option.title}
+            options={options}
+            loading={loading}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Asynchronous"
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <React.Fragment>
+                      {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                      {params.InputProps.endAdornment}
+                    </React.Fragment>
+                  ),
+                }}
+              />
+            )}
+          />
+        </Grid>
+
+      </Grid>
   );
 }
 

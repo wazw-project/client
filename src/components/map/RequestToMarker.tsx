5ca78835ab537c6dc1933f610439a110298ead88
naamafrank1 <naama0556772275@gmail.com>
2022-08-30 22:38:43 +0300

    request not close

--- src/components/map/RequestToMarker.tsx
+++ src/components/map/RequestToMarker.tsx
@@ -109,7 +109,7 @@ const Request = () => {
             }
             handleClose();
             setOpen(false);
-            requestStore.currentRequestAddressesName="";
+            // requestStore.currentRequestAddressesName="";
         }
     }
 

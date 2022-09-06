52ffe2926a20e02b7fbcc3dd1983248119a117eb
naamafrank1 <naama0556772275@gmail.com>
2022-09-06 21:07:50 +0300

    card selected

--- src/components/map/cardOfSelected.tsx
+++ src/components/map/cardOfSelected.tsx
@@ -7,29 +7,64 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { observer } from 'mobx-react-lite';
 import markerStore from '../../store/markerStore';
+import Man2Icon from '@mui/icons-material/Man2';
+import DescriptionIcon from '@mui/icons-material/Description';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import MailIcon from '@mui/icons-material/Mail';
+import Grid from '@mui/material/Grid';
 
-
-const CardOfSelected=()=> {
+const CardOfSelected = () => {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275,textAlign:'center' }}>
+
       <CardContent>
-      <Typography variant="h5" component="div">
-        {markerStore.currentMarker.name}
+        <Typography sx={{ color: '#FF0000' }} variant="h4" component="div">
+          details:
         </Typography>
-        <Typography variant="h5" component="div">
-        {markerStore.currentMarker.description}
-        </Typography>
-        <Typography variant="h5" component="div">
-        {markerStore.currentMarker.phone}
-        </Typography>
-        <Typography variant="h5" component="div">
-        {markerStore.currentMarker.email}
+        
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid sx={{height:'50%',marginLeft:'5%',marginTop:'3%'}} >
+            <Man2Icon />
+          </Grid>
+          <Grid item >
+            <Typography variant="h5" component="div">
+              {markerStore.currentMarker.name}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid sx={{height:'50%',marginLeft:'5%',marginTop:'3%'}} >
+            <DescriptionIcon />
+          </Grid>
+          <Grid item >
+            <Typography variant="h5" component="div">
+            {markerStore.currentMarker.description}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid sx={{height:'50%',marginLeft:'5%',marginTop:'3%'}} >
+            <PhoneIphoneIcon />
+          </Grid>
+          <Grid item >
+            <Typography variant="h5" component="div">
+            {markerStore.currentMarker.phone}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid sx={{height:'50%',marginLeft:'5%',marginTop:'3%'}} >
+            <MailIcon />
+          </Grid>
+          <Grid item >
+            <Typography variant="h5" component="div">
+            {markerStore.currentMarker.email}
+            </Typography>
+          </Grid>
+        </Grid>
       
-        </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+    
     </Card>
   );
 }
